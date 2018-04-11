4b54df263e23c2b3edc3837b582c058fd38dcd66
Finn Thye <piqueen@gmail.com>
2018-04-11 10:00:04 -0600

    Progress with Weather project

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'd2d2c3d2f8f47b79ea1e911daad12bc4';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+}
